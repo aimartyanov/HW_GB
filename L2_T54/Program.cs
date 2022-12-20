19091914bbf5ee4e473dc05fb64a3e4cb7f849d5
aimartyanov <aimartyanov@gmail.com>
2022-12-20 12:30:54 +0300

    Выполненное домашнее

--- L2_T54/Program.cs
+++ L2_T54/Program.cs
@@ -7,3 +7,55 @@
 7 4 2 1
 9 5 3 2
 8 4 4 2*/
+Console.Write("Введите размер ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер ");
+int n = Convert.ToInt32(Console.ReadLine());
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 20);
+        }
+    }
+
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+void SortArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] >= array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+
+                }
+            }
+        }
+    }
+}
+SortArr(array);
+PrintArray(array);
