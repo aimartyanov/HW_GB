faec91ea8f2f0d5d15d7e82d6a960f5954d9da78
aimartyanov <aimartyanov@gmail.com>
2023-03-03 16:48:57 +0300

    3

--- J_L1_T3/src/Main.java
+++ J_L1_T3/src/Main.java
@@ -1,17 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+8.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        var a = reader.readLine();
+        var b = reader.readLine();
+        var c = reader.readLine();
+        double monthString = switch (c)
+        {
+            case "+" -> a + b;
+            case "-" -> "February";
+            case "/" -> "March";
+            case "*" -> "April";
+            case "%" -> "May";
+            case "//" -> "June";
+            case "sqr" -> "July";
+            case "sqrt" -> "August";
+            default -> "Invalid operator";
+        };
+        System.out.println(monthString);
     }
 }
