dfb352fddd34c464d96b36d329f609193dd39a89
aimartyanov <aimartyanov@gmail.com>
2023-03-03 14:51:58 +0300

    2

---
+++ J_L1_T2/src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean isPrime = true;
+        for (int i = 2; i <= 100; i++)
+        {
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                System.out.println(i);
+        }
+    }
+}
