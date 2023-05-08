e5fcdf7223f1f607f447c5810c82e0f06139870b
aimartyanov <aimartyanov@gmail.com>
2023-05-08 17:39:06 +0300

    arch

--- OOP_L5_T1/src/FileIO.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public interface FileIO
-{
-    void save(FamilyTree familyTree, String fileName) throws IOException;
-
-    FamilyTree load(String fileName) throws IOException, ClassNotFoundException;
-}
--- OOP_L5_T1/src/GenealogyTreeIO.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public interface GenealogyTreeIO
-{
-    void save(GenealogyTree tree, String fileName) throws IOException;
-
-    GenealogyTree load(String fileName) throws IOException;
-}
